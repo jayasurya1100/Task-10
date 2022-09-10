b011f639eecebf0a14acce699a6b76e0e050aacf
jayasurya <80097452+jayasurya1100@users.noreply.github.com>
2022-09-10 17:13:54 +0530

    Add files via upload

---
+++ m.js
@@ -0,0 +1,55 @@
+setTimeout(()=>{
+    var a=document.querySelector('#box')
+    a.innerHTML=10
+    setTimeout(()=>{
+    
+        a.innerHTML=9
+    
+    setTimeout(()=>{
+        
+        a.innerHTML=8
+    
+    setTimeout(()=>{
+       
+        a.innerHTML=7
+    
+    setTimeout(()=>{
+        
+        a.innerHTML=6
+    
+    setTimeout(()=>{
+        
+        a.innerHTML=5
+    
+    setTimeout(()=>{
+        
+        a.innerHTML=4
+    
+    setTimeout(()=>{
+       
+        a.innerHTML=3
+    
+    setTimeout(()=>{
+        
+        a.innerHTML=2
+    
+    setTimeout(()=>{
+        
+        a.innerHTML=1
+    
+    setTimeout(()=>{
+       
+        a.innerHTML="Happy ✝ ☪ 🕉 Independence Day  "
+    },1000)
+},1000)
+   
+},1000)
+
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
